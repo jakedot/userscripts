f89dd087f4e105f7fd08a92126719bd09fc5d22e
Jakob Mayer <mail@jakobmayer.at>
2016-10-27 12:45:21 +0200

    efn use raw urls

--- efn.meta.js
+++ efn.meta.js
@@ -3,8 +3,8 @@
 // @description	Makes field notes usable in other places: shows already found caches from there in your statistics, and changes the display of caches in bookmark lists as if they were already logged as found.
 // @author		Jakob Mayer <mail@jakobmayer.at>
 // @namespace   https://github.com/jakedot/userscripts/
-// @updateURL   https://github.com/jakedot/userscripts/efn.meta.js
-// @downloadURL https://github.com/jakedot/userscripts/efn.user.js
+// @updateURL   https://raw.githubusercontent.com/jakedot/userscripts/master/efn.meta.js
+// @downloadURL https://raw.githubusercontent.com/jakedot/userscripts/master/efn.user.js
 // @icon        https://www.geocaching.com/images/icons/32/write_log.png
 // @include     https://www.geocaching.com/my/statistics.aspx
 // @include     http://www.geocaching.com/my/statistics.aspx
--- efn.user.js
+++ efn.user.js
@@ -3,14 +3,14 @@
 // @description	Makes field notes usable in other places: shows already found caches from there in your statistics, and changes the display of caches in bookmark lists as if they were already logged as found.
 // @author		Jakob Mayer <mail@jakobmayer.at>
 // @namespace   https://github.com/jakedot/userscripts/
-// @updateURL   https://github.com/jakedot/userscripts/efn.meta.js
-// @downloadURL https://github.com/jakedot/userscripts/efn.user.js
+// @updateURL   https://raw.githubusercontent.com/jakedot/userscripts/master/efn.meta.js
+// @downloadURL https://raw.githubusercontent.com/jakedot/userscripts/master/efn.user.js
 // @icon        https://www.geocaching.com/images/icons/32/write_log.png
 // @include     https://www.geocaching.com/my/statistics.aspx
 // @include     http://www.geocaching.com/my/statistics.aspx
 // @include     https://www.geocaching.com/bookmarks/*
 // @include     http://www.geocaching.com/bookmarks/*
-// @version     1.1
+// @version     1.2
 // @require		https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js
 // @grant       none
 // @grant		GM_xmlhttpRequest
